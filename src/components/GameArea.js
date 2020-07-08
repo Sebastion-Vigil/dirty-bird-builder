9d749e03661181412ae0e9251c19c17085062701
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-07-08 06:11:08 -0500

    future plans

--- src/components/GameArea.js
+++ src/components/GameArea.js
@@ -6,6 +6,15 @@ import AnswerScreen from './AnswerScreen.js'
 
 import '../css/GameArea.css'
 
+// here is where I should handle => 
+// user greeted with welcome message and root menu
+// upon clicking menu item, user sent to <GameScreen/>
+// => make button that quits game and returns to root menu
+// each menu item button passes unique props to the game screen:
+    // question
+    // number of droppads
+    // number of tile ingredients
+    // path to corresponding tile images
 class GameArea extends React.Component {
     render() {
         return (
