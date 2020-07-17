064bcac280008092999d191f3aef457bc68cc115
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-07-17 05:30:36 -0500

    fixed the SesameTop.png import problem; was missing a "/" before "assets" in the img path. Damn that was aggravating

--- src/components/SelectBun.js
+++ src/components/SelectBun.js
@@ -4,20 +4,20 @@ import '../css/Select.css'
 
 // import whole bun images for button backgrounds
 import sesameWhole from '../assets/buns/SesameWhole.png'
-// import sesameTop from '..assets/buns/SesameTop.png'
-// import sesameBottom from '..assets/buns/SesameBottom.png'
+import sesameTop from '../assets/buns/SesameTop.png'
+import sesameBottom from '../assets/buns/SesameBottom.png'
 import jalapenoWhole from '../assets/buns/JalapenoWhole.png'
-// import jalapenoTop from '../assets/buns/JalapenoTop.png'
-// import jalapenoBottom from '../assets/buns/JalapenoBottom.png'
+import jalapenoTop from '../assets/buns/JalapenoTop.png'
+import jalapenoBottom from '../assets/buns/JalapenoBottom.png'
 import onionWhole from '../assets/buns/OnionWhole.png'
-// import onionTop from '../assets/buns/OnionTop.png'
-// import onionBottom from '../assets/buns/OnionBottom.png'
+import onionTop from '../assets/buns/OnionTop.png'
+import onionBottom from '../assets/buns/OnionBottom.png'
 import briocheWhole from '../assets/buns/BriocheWhole.png'
-// import briocheTop from '../assets/buns/BriocheTop.png'
-// import briocheBottom from '../assets/buns/BriocheBottom.png'
+import briocheTop from '../assets/buns/BriocheTop.png'
+import briocheBottom from '../assets/buns/BriocheBottom.png'
 import wheatWhole from '../assets/buns/WheatWhole.png'
-// import wheatTop from '../assets/buns/WheatTop.png'
-// import wheatBottom from '../assets/buns/WheatBottom.png'
+import wheatTop from '../assets/buns/WheatTop.png'
+import wheatBottom from '../assets/buns/WheatBottom.png'
 
 class SelectBun extends React.Component {
     
@@ -31,7 +31,7 @@ class SelectBun extends React.Component {
               Select Bun
               <div 
                 className='select-button'
-                // onClick={() => this.chooseBun([sesameTop, sesameBottom], 'sesame')}
+                onClick={() => this.chooseBun([sesameTop, sesameBottom], 'sesame')}
                 style={{
                     backgroundImage: 'url(' + sesameWhole + ')',
                     backgroundSize: '100% 100%'
@@ -39,7 +39,7 @@ class SelectBun extends React.Component {
               ></div>
               <div 
                 className='select-button'
-                // onClick={() => this.chooseBun([jalapenoTop, jalapenoBottom]), 'jalapeno'}
+                onClick={() => this.chooseBun([jalapenoTop, jalapenoBottom], 'jalapeno')}
                 style={{
                     backgroundImage: 'url(' + jalapenoWhole + ')',
                     backgroundSize: '100% 100%'
@@ -47,7 +47,7 @@ class SelectBun extends React.Component {
               ></div>
               <div 
                 className='select-button'
-                // onClick={() => this.chooseBun([onionTop, onionBottom], 'onion-bun')}
+                onClick={() => this.chooseBun([onionTop, onionBottom], 'onion-bun')}
                 style={{
                     backgroundImage: 'url(' + onionWhole + ')',
                     backgroundSize: '100% 100%'
@@ -55,7 +55,7 @@ class SelectBun extends React.Component {
               ></div>
               <div 
                 className='select-button'
-                // onClick={() => this.chooseBun([briocheTop, briocheBottom], 'brioche')}
+                onClick={() => this.chooseBun([briocheTop, briocheBottom], 'brioche')}
                 style={{
                     backgroundImage: 'url(' + briocheWhole + ')',
                     backgroundSize: '100% 100%'
@@ -63,7 +63,7 @@ class SelectBun extends React.Component {
               ></div>
               <div 
                 className='select-button'
-                // onClick={() => this.chooseBun([wheatTop, wheatBottom], 'wheat')}
+                onClick={() => this.chooseBun([wheatTop, wheatBottom], 'wheat')}
                 style={{
                     backgroundImage: 'url(' + wheatWhole + ')',
                     backgroundSize: '100% 100%'
