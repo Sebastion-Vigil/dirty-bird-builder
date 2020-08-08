a6e6e1f93d592f3d1f092d7eb17913de2699b592
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-08-07 19:35:26 -0500

    renamed AnswerScreen.js to Answer.js -> restructuring directory layout of project, one step at a time

---
+++ src/components/Answer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../css/Answer.css'
+
+class Answer extends React.Component {
+    render() {
+        return (
+            <div className='answer'></div>
+        )
+    }
+}
+
+export default Answer
--- src/components/GameArea.js
+++ src/components/GameArea.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Logo from './Logo.js'
 import GameScreen from './GameScreen.js'
-import AnswerScreen from './AnswerScreen.js'
+import Answer from './Answer.js'
 
 // import '../css/GameArea.css'
 
@@ -14,7 +14,7 @@ class GameArea extends React.Component {
                 <GameScreen
                   gameData={this.props.gameData} 
                 />
-                <AnswerScreen />
+                <Answer />
             </div>
         )
     }
